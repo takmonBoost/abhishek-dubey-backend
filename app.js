5915d9d1e7d7d102a31f5ba90e23820d4f81cce9
Ravi <ravisaini@gmail.com>
2024-12-09 15:09:41 +0530

    changes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:5173/",
+    origin: "*",
     credentials: true,
     methods: ['GET', 'POST'],
   })
